48c9d85272875c5b9a4c5a2d4d54819d42a73882
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-02-10 03:53:48 +0500

    Rename projects&files

---
+++ HovyFridge/app/src/main/java/com/niklyadov/hovy_fridge_app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.niklyadov.hovy_fridge_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.niklyadov.hovy_fridge_app.Entity.Student
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val view = findViewById<TextView>(R.id.textView)
+        view.text = Student("Иван", false).toString();
+
+            }
+}
