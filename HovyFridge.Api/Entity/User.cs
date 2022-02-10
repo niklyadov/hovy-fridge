d1f408f828f5ff838495517a27ed70644083d74e
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-02-10 16:56:42 +0500

    Added favorite fridge

--- HovyFridge.Api/Entity/User.cs
+++ HovyFridge.Api/Entity/User.cs
@@ -3,8 +3,9 @@
     public class User : IEntity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Token { get; set; }
+        public string? Name { get; set; }
+        public string? Token { get; set; }
         public UserRole Role { get; set; } = UserRole.Common;
+        public Fridge? FavoriteFridge { get; set; }
     }
 }
